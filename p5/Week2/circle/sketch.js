8f409c184f36be13c13a78acfce70fc2230c505d
GenGames <48166284+GenGames@users.noreply.github.com>
2020-01-21 09:24:31 -0600

    Added Week 2

---
+++ p5/Week2/circle/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  // put setup code here
+  createCanvas(500, 500);
+}
+
+function draw() {
+  // put drawing code here
+
+  noStroke();
+  fill('red');
+  ellipse(mouseX, mouseY, 10, 10) ;
+  //background(150);
+}
