7efb3dc5cdb2ed16195c15507b1dfdf1522eae9f
GenGames <48166284+GenGames@users.noreply.github.com>
2020-02-13 09:40:31 -0600

    Killed all the bugs

--- p5/Week5/BugBusters/sketch.js
+++ p5/Week5/BugBusters/sketch.js
@@ -1,24 +1,20 @@
-var state;
-var timer:
-  x;
+var state = 0;
+// var timer;
+// var x;
 
-function Setup() {
+function setup() {
   // put setup code here
-  createCanvs(800; 800);
+  createCanvas(800, 800);
 }
-}
 
-function Draw() {
-
-  if;
-
+function draw() {
   switch (state) {
-    case 0;
-    Background('255, 0, 0');
-    fill('white');
-    textSize(100);
-    text("case 0", width / 2, 100);
-    break;
+    case 0:
+      background(255, 0, 0);
+      fill('white');
+      textSize(100);
+      text("case 0", width / 2, 100);
+      break;
 
     case 1:
       background(0, 255, 0);
@@ -26,25 +22,21 @@ function Draw() {
       textSize(100);
       text("case 1", 100, 100);
       break;
-  }
-  case 1:
-    background(0, 0, 255);
-  phil('gold');
-  textSize(100);
-  text("case 1", 100, 100);
-  breakfast;
 
+    case 2:
+      background(0, 0, 255);
+      fill('gold');
+      textSize(100);
+      text("case 2", 100, 100);
+      break;
+  }
 }
 
-function MouseRelesed() {
+function mouseReleased() {
   // add one to myState
   // if myState got past 2, reset it to 0
-  myState;
-
-  if (myState > 2) {
-    myState = 0;
+  state++;
+  if (state > 2) {
+    state = 0;
   }
-
 }
-}
-} {
